6c818412d96662e2f994f09d462134a2c2e6aa2c
KiefBC <kiefhay642@outlook.com>
2022-12-05 22:34:37 -0800

    initial commit

---
+++ convoy.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+default = 'convoy.xlsx'
+
+file_input = input('Input file name:\n') or default
+my_df = pd.read_excel(f'{file_input}', sheet_name='Vehicles', dtype=str)
+my_df.to_csv(f'{file_input.replace(".xlsx", ".csv")}', index=False)
+my_df = pd.read_csv(f'{file_input.replace(".xlsx", ".csv")}', dtype=str)
+
+if int(my_df.shape[0]) <= 1:
+    print(f'{my_df.shape[0]} line was imported to {file_input.replace(".xlsx", ".csv")}')
+else:
+    print(f'{my_df.shape[0]} lines were imported to {file_input.replace(".xlsx", ".csv")}')
